c766ea700d210f1eea86e1e0799163769a336188
RazielLimaa <razi30216@gmail.com>
2025-10-04 19:53:27 -0300

    first commit

--- api.py
+++ api.py
@@ -456,7 +456,7 @@ if __name__ == "__main__":
     # Roda servidor
     uvicorn.run(
         app,
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=5000,
         log_level="info"
     )
